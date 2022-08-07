d0ae1748f4f921d25411530517adc3fee344d488
Muhammad Umar <umarsadiqi404@gmail.com>
2022-08-07 11:51:04 +0500

    Updated index.js, react-redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import App from "./components/App";
+import reducers from "./reducers";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={createStore(reducers)}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+export default combineReducers({
+    // Add your reducers here
+});
