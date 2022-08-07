f627d13fa1027a6a1abc5d4f05f36a3113c19f0d
Muhammad Umar <umarsadiqi404@gmail.com>
2022-08-07 13:23:40 +0500

    applied middleware (thunk) 4 api request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
-export const fetchPosts = () => {
-    return {
-        type: "FETCH_POSTS",
-    };
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPosts = () => async (dispatch) => {
+    const response = await jsonPlaceholder.get("/posts");
+    dispatch({ type: "FETCH_POSTS", payload: response });
 };
+
+// export const fetchPosts = () => {
+//     return async function (dispatch, getState) {
+//         const response = await jsonPlaceholder.get("/posts");
+//         dispatch({ type: "FETCH_POSTS", payload: response, });
+//     };
+// };
--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
+const store = createStore(reducers, applyMiddleware(thunk));
+
 const root = createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <Provider store={createStore(reducers)}>
+        <Provider store={store}>
             <App />
         </Provider>
     </React.StrictMode>
