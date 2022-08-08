403c874cadda4f6a4dd05b9f1845c10760d9f766
Muhammad Umar <umarsadiqi404@gmail.com>
2022-08-08 23:36:30 +0500

    memoized fetchUser action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async (dispatch) => {
@@ -8,13 +10,11 @@ export const fetchPosts = () => async (dispatch) => {
     });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
-    dispatch({
-        type: "FETCH_USER",
-        payload: response.data,
-    });
-};
+    dispatch({ type: "FETCH_USER", payload: response.data });
+});
 
 // export const fetchPosts = () => {
 //     return async function (dispatch, getState) {
