e34e28d257065fd8f348c61a95197220ea31547e
Muhammad Umar <umarsadiqi404@gmail.com>
2022-08-10 09:28:55 +0500

    _.chain refactor

--- src/actions/index.js
+++ src/actions/index.js
@@ -17,8 +17,13 @@ export const fetchUser = (id) => async (dispatch) => {
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
     await dispatch(fetchPosts());
-    const userIds = _.uniq(_.map(getState().posts, "userId"));
-    userIds.forEach((id) => dispatch(fetchUser(id)));
+    // const userIds = _.uniq(_.map(getState().posts, "userId"));
+    // userIds.forEach((id) => dispatch(fetchUser(id)));
+    _.chain(getState().posts)
+        .map("userId")
+        .uniq()
+        .forEach((id) => dispatch(fetchUser(id)))
+        .value();
 };
 
 /////// MEMOIZATION ///////
